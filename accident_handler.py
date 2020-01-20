358bf4290218942cc9af368e35970ca172f8ef8c
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-01-19 19:33:42 -0700

    ver 1.0.3

---
+++ accident_handler.py
@@ -0,0 +1,55 @@
+import json
+import boto3
+import base64
+import datetime
+
+exact_date=datetime.datetime.now()
+parent=boto3.client('cognito-idp')
+exact_date=exact_date.strftime('%x')
+
+dynamodb=boto3.resource('dynamodb')
+table=dynamodb.Table('Accident_Case')
+s3=boto3.client('s3')
+invoke_lambda=boto3.client('lambda')
+
+
+ 
+def lambda_handler(event, context):
+    # TODO implement
+    #the eventpart is with devansh by react native . 
+    
+    latitude=event['loc']['lat']
+    longitude=event['loc']['long']
+    device_id=event['deviceId']
+    AUID=device_id+latitude+longitude+exact_date
+    
+    video_64_encode=event['video']
+    
+    video_64_encode=str.encode(video_64_encode)
+    
+    video_64_decode=base64.encodestring(video_64_encode)
+    
+    with open("/tmp/log.mp4","wb") as f:
+        f.write(video_64_decode)
+    s3.upload_file("/tmp/log.mp4","bucket2.sih","{}.mp4".format(AUID))
+    
+    #enter info in dynamo db table
+    s3url='https://s3.amazonaws.com/bucket2.sih/'+'{}.mp4'.format(AUID)
+    table.put_item(Item=json.dumps({'id':AUID,'lat':latitude,
+    'long':longitude,'googleAdd':'https://www.google.com/maps/place/{},{}'.format(latitude,longitude)},'videoUrl':s3url))
+    
+    
+    #Invocation of the lambda function from one lambda functions the other lambda function return the partner id 's 
+    
+    
+    #payload={"message":"hi you have been invoked ."}
+    #resp=invoke_lambda.invoke(FunctionName='partner_list',InvocationType='Event',Payload=json.dumps(payload))
+    
+    
+    #sending push notification to the partnerlist
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
